0caefa70a466b9fd6442cf2e369dc6a98a17ea26
chim347 <nhanntse160792@gmail.com>
2024-02-03 02:54:22 +0700

    2/3/2024

--- src/pages/ChuanDoan/index.js
+++ src/pages/ChuanDoan/index.js
@@ -1,35 +1,9 @@
 import classNames from 'classnames/bind';
 import styles from './ChuanDoan.module.scss';
 import images from '~/assets/images';
-import { useRef, useState } from 'react';
-import { Wrapper as PopperWrapper } from '~/components/Popper';
-import Tippy from '@tippyjs/react';
 
 const cx = classNames.bind(styles);
 function ChuanDoan() {
-    const [searchValue, setSearchValue] = useState('');
-    const [searchResult, setSearchResult] = useState([]);
-    const [showResult, setShowResult] = useState(false);
-    const [loading, setLoading] = useState(false);
-
-    const inputRef = useRef();
-
-    const handleClear = () => {
-        setSearchValue('');
-        setSearchResult([]);
-        inputRef.current.focus();
-    };
-
-    const handleHideResult = () => {
-        setShowResult(false);
-    };
-
-    const handleChange = (e) => {
-        const searchValue = e.target.value;
-        if (!searchValue.startsWith(' ')) {
-            setSearchValue(searchValue);
-        }
-    };
     return (
         <div className={cx('rectangle')}>
             <p className={cx('text-wrapper-header')}>
@@ -38,21 +12,12 @@ function ChuanDoan() {
                 Khẩn Cấp SOS khuyến khích người bệnh đến trung tâm y tế để kiểm tra sức khỏe một cách chuẩn xác nhất.
             </p>
 
-            <Tippy
-                interactive
-                visible={showResult && searchResult.length > 0}
-                render={(attrs) => (
-                    <div className={cx('search-result')} tabIndex="-1" {...attrs}>
-                        <PopperWrapper>
-                            <h4 className={cx('search-title')}>Accounts</h4>
-                        </PopperWrapper>
-                    </div>
-                )}
-                className={cx('box-search')}
-            >
-                <img className={cx('icon-search')} alt="IconSearch" src={images.iconSearchWhiteColor} />
-                <p className={cx('text-wrapper-search')}>Chọn các triệu chứng bạn đang mắc phải</p>
-            </Tippy>
+            <div className={cx('box-search')}>
+                <button className={cx('icon-search')}>
+                    <img alt="IconSearch" src={images.iconSearchWhiteColor} />
+                </button>
+                <input placeholder="Chọn các triệu chứng bạn đang mắc phải" spellCheck={false} />
+            </div>
 
             <div className={cx('search-button')}>
                 <img className={cx('icon-button-search')} alt="IconSearch" src={images.iconSearchWhiteColor} />
