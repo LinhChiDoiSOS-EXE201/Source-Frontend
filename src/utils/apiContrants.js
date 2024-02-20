e3753b971b62f7d2928e86518b0253183cd0cbaa
chim347 <nhanntse160792@gmail.com>
2024-02-20 16:42:13 +0700

    changePassword

--- src/utils/apiContrants.js
+++ src/utils/apiContrants.js
@@ -11,3 +11,10 @@ export const GETALLCOURSE = 'api/v1/course';
 //chuan doan
 export const GETALLCATEGORY = '/api/v1/categorykeyword/list-keyword';
 export const TRACUU = '/api/v1/chuandoan/by-list-keyword-id';
+export const GETALLCOURSE = '/api/v1/course/all';
+
+//reset-password
+export const RESETPASSWORD = '/api/v1/auth/reset-password';
+
+// change-password
+export const CHANGEPASSWORD = '/api/v1/auth/change-password';
