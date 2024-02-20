21c741ad4d5564c97fb3c291500f3d25d229aac4
chim347 <nhanntse160792@gmail.com>
2024-02-20 16:42:13 +0700

    changePassword

--- src/utils/apiContrants.js
+++ src/utils/apiContrants.js
@@ -19,3 +19,9 @@ export const RESETPASSWORD = '/api/v1/auth/reset-password';
 // change-password
 export const CHANGEPASSWORD = '/api/v1/auth/change-password';
 export const GETALLCOURSE = '/api/v1/course/all';
+
+//reset-password
+export const RESETPASSWORD = '/api/v1/auth/reset-password';
+
+// change-password
+export const CHANGEPASSWORD = '/api/v1/auth/change-password';
