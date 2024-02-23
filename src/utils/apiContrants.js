3be77e42cc13fba642590aa85248af9347444c69
chim347 <nhanntse160792@gmail.com>
2024-02-23 22:38:17 +0700

    api

--- src/utils/apiContrants.js
+++ src/utils/apiContrants.js
@@ -5,19 +5,9 @@ export const REGISTER = '/api/v1/customers/register';
 //getCustomer By Id
 export const USERDETAIL = '/api/v1/customers';
 
-//category
-export const GETALLCOURSE = 'api/v1/course';
-
 //chuan doan
 export const GETALLCATEGORY = '/api/v1/categorykeyword/list-keyword';
 export const TRACUU = '/api/v1/chuandoan/by-list-keyword-id';
-export const GETALLCOURSE = '/api/v1/course/all';
-
-//reset-password
-export const RESETPASSWORD = '/api/v1/auth/reset-password';
-
-// change-password
-export const CHANGEPASSWORD = '/api/v1/auth/change-password';
 export const GETALLCOURSE = '/api/v1/course/all';
 
 //reset-password
